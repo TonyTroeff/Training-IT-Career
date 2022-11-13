273a13cf4e59fa69470bbd687e09949de7f919cb
Tony Troeff <tonyslav_troeff@outlook.com>
2022-11-13 15:26:35 +0200

    Solved the Circle area and perimeter task

---
+++ Programming basics/CircleAreaPerimeter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CircleAreaPerimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * r * r;
+            double perimeter = 2 * Math.PI * r;
+
+            Console.WriteLine($"Area = {Math.Round(area, 2)}");
+            Console.WriteLine($"Perimeter = {Math.Round(perimeter, 2)}");
+        }
+    }
+}
