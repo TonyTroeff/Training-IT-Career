51ef5659645d6c82c2cac27dac34a59c4cf094c1
Tony Troeff <tonyslav_troeff@outlook.com>
2022-11-27 12:42:42 +0200

    Solved the first two tasks from the `Exam 1` used for preparation

---
+++ Programming basics/Exam preparation/Exam1_TrainingLab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exam1_TrainingLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double height = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+
+            double rows = Math.Truncate((width * 100 - 100) / 70);
+            double cols = Math.Truncate(height * 100 / 120);
+
+            double ans = rows * cols - 3;
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ Programming basics/Exam preparation/Exam1_TransportPrice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exam1_TransportPrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kilometers = int.Parse(Console.ReadLine());
+            string partOfDay = Console.ReadLine();
+
+            double totalPrice = 0;
+            if (kilometers < 20)
+            {
+                double taxiPrice = 0;
+                if (partOfDay == "day") taxiPrice = 0.79;
+                else if (partOfDay == "night") taxiPrice = 0.9;
+
+                totalPrice = 0.7 + taxiPrice * kilometers;
+            }
+            else if (kilometers < 100)
+            {
+                totalPrice = 0.09 * kilometers;
+            }
+            else
+            {
+                totalPrice = 0.06 * kilometers;
+            }
+
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
