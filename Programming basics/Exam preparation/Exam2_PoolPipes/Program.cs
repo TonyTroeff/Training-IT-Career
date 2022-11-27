bbf49a459a4a01d4eba43b8dc5c68bef74b99f1b
Tony Troeff <tonyslav_troeff@outlook.com>
2022-11-27 18:12:08 +0200

    Started solving tasks from `Exam 2`

---
+++ Programming basics/Exam preparation/Exam2_PoolPipes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exam2_PoolPipes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int volume = int.Parse(Console.ReadLine());
+            int pipe1Debit = int.Parse(Console.ReadLine());
+            int pipe2Debit = int.Parse(Console.ReadLine());
+            double hours = double.Parse(Console.ReadLine());
+
+            double pipe1Fill = pipe1Debit * hours;
+            double pipe2Fill = pipe2Debit * hours;
+            double totalFill = pipe1Fill + pipe2Fill;
+
+            if (totalFill > volume)
+            {
+                Console.WriteLine($"For {hours} hours the pool overflows with {totalFill - volume:f1} liters.");
+            }
+            else
+            {
+                double fillPercent = totalFill / volume * 100;
+                double pipe1FillPercent = pipe1Fill / totalFill * 100;
+                double pipe2FillPercent = pipe2Fill / totalFill * 100;
+                
+                Console.WriteLine($"The pool is {Math.Truncate(fillPercent)}% full. Pipe 1: {Math.Truncate(pipe1FillPercent)}%. Pipe 2: {Math.Truncate(pipe2FillPercent)}%.");
+            }
+        }
+    }
+}
---
+++ Programming basics/Loops/Exam2_MagicCombinations/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exam2_MagicCombinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            for (int i1 = 1; i1 <= 9; i1++)
+            {
+                for (int i2 = 1; i2 <= 9; i2++)
+                {
+                    for (int i3 = 1; i3 <= 9; i3++)
+                    {
+                        for (int i4 = 1; i4 <= 9; i4++)
+                        {
+                            for (int i5 = 1; i5 <= 9; i5++)
+                            {
+                                for (int i6 = 1; i6 <= 9; i6++)
+                                {
+                                    int product = i1 * i2 * i3 * i4 * i5 * i6;
+                                    if (product == magicNumber)
+                                        Console.WriteLine($"{i1}{i2}{i3}{i4}{i5}{i6}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
