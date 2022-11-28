106972d1c96ec3351fd70078e9495e575ac2bef8
Tony Troeff <tonyslav_troeff@outlook.com>
2022-11-28 23:20:06 +0200

    Solved the `Hotel room` task from `Exam 5`

---
+++ Programming basics/Exam preparation/Exam5_HotelRoom/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exam5_HotelRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int nights = int.Parse(Console.ReadLine());
+
+            double apartmentPrice = 0, studioPrice = 0;
+            if (month == "May" || month == "October")
+            {
+                studioPrice = 50 * nights;
+                apartmentPrice = 65 * nights;
+
+                if (nights > 14) studioPrice *= 0.70;
+                else if (nights > 7) studioPrice *= 0.95;
+            }
+            else if (month == "June" || month == "September")
+            {
+                studioPrice = 75.2 * nights;
+                apartmentPrice = 68.7 * nights;
+
+                if (nights > 14) studioPrice *= 0.8;
+            }
+            else if (month == "July" || month == "August")
+            {
+                studioPrice = 76 * nights;
+                apartmentPrice = 77 * nights;
+            }
+
+            if (nights > 14) apartmentPrice *= 0.9;
+
+            Console.WriteLine($"Apartment: {apartmentPrice:f2} lv.");
+            Console.WriteLine($"Studio: {studioPrice:f2} lv.");
+        }
+    }
+}
