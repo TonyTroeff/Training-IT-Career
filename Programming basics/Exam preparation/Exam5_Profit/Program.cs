1d0973c54f61f5ec882d4ef6fb6a2b1d202d2ff9
Tony Troeff <tonyslav_troeff@outlook.com>
2022-11-28 23:09:59 +0200

    Fixed the `Profit` task from `Exam 5`

---
+++ Programming basics/Exam preparation/Exam5_Profit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exam5_Profit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int workDays = int.Parse(Console.ReadLine());
+            double wagePerDay = double.Parse(Console.ReadLine());
+            double dollarRate = double.Parse(Console.ReadLine());
+
+            double monthlyWage = workDays * wagePerDay;
+            double incomePerYear = 14.5 * monthlyWage;
+            double taxes = 0.25 * incomePerYear;
+
+            double netIncome = (incomePerYear - taxes) * dollarRate;
+            double incomePerDay = netIncome / 365;
+            Console.WriteLine($"{incomePerDay:f2}");
+        }
+    }
+}
