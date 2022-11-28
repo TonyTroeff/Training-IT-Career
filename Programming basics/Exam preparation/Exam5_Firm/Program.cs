e2801133a0754729ef9ddfc456a9b09cd8866f12
Tony Troeff <tonyslav_troeff@outlook.com>
2022-11-28 23:15:03 +0200

    Solved the `Firm` task from `Exam 5`

---
+++ Programming basics/Exam preparation/Exam5_Firm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exam5_Firm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+
+            int availableHours = (int)Math.Floor(days * 8 * 0.9 + workers * days * 2);
+            if (availableHours >= hours) Console.WriteLine($"Yes!{availableHours - hours} hours left.");
+            else Console.WriteLine($"Not enough time!{hours - availableHours} hours needed.");
+        }
+    }
+}
