26fb458cccfac1502781b24ecba7796bfc050ea2
Tony Troeff <tonyslav_troeff@outlook.com>
2022-11-29 18:38:02 +0200

    Solved the `Exchange` task from `Exam Afternoon`

---
+++ Programming basics/Exam preparation/Afternoon_Exchange/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Afternoon_Exchange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dollars = double.Parse(Console.ReadLine());
+            double convertRate = double.Parse(Console.ReadLine());
+            int satoshiPerByte = int.Parse(Console.ReadLine());
+
+            double availableBitcoins = dollars / convertRate;
+            double tax = availableBitcoins * (satoshiPerByte * 1024) / 100000000;
+            double taxInDollars = tax * convertRate;
+
+            double boughtBitcoins = availableBitcoins - tax;
+            double programmerPayment = boughtBitcoins * 0.1;
+
+            double totalIncome = boughtBitcoins - programmerPayment;
+                
+            Console.WriteLine($"Total bitcoin after expenses: {totalIncome:f5} BTC");
+            Console.WriteLine($"Tax payed: {taxInDollars:f2} USD");
+            Console.WriteLine($"Programmer's payment: {programmerPayment:f5} BTC");
+        }
+    }
+}
