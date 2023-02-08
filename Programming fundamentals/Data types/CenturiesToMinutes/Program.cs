31529ee97e5873f77051a10023bd3b0357c80573
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-08 17:32:11 +0200

    Solved the `Centuries to minutes` problem

---
+++ Programming fundamentals/Data types/CenturiesToMinutes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CenturiesToMinutes
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days = (int)Math.Floor(years * 365.2422);
+            int hours = days * 24;
+            int minutes = hours * 60;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
