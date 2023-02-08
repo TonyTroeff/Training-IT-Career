2d76f5364c02cd1ddcd6b7e943dca55dad59a7c0
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-08 17:49:31 +0200

    Solved the `Rectangle` problem

---
+++ Programming fundamentals/Data types/Rectangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Rectangle
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            double perimeter = 2 * (a + b);
+            double area = a * b;
+            double diagonal = Math.Sqrt(a * a + b * b);
+
+            Console.WriteLine(perimeter);
+            Console.WriteLine(area);
+            Console.WriteLine(diagonal);
+        }
+    }
+}
