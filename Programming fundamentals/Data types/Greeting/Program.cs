1a8912c6610681407a1f07c722b59e046bd82ab9
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-08 23:12:57 +0200

    Solved the `Greeting` problem

---
+++ Programming fundamentals/Data types/Greeting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Greeting
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Hello, {firstName} {lastName}.");
+            Console.WriteLine($"You are {age} years old.");
+        }
+    }
+}
