420ef8f5af65290c72e39cdc36c9ac3a50a42154
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-08 17:40:59 +0200

    Solved the `Convert to hexadecimal and binary` problem

---
+++ Programming fundamentals/Data types/ConvertToHexadecimalAndBinary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConvertToHexadecimalAndBinary
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(number, 16).ToUpper());
+            Console.WriteLine(Convert.ToString(number, 2));
+        }
+    }
+}
