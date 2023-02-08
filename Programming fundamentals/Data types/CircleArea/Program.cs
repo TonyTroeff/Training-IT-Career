642bcbe997908cfc3640ec68e4698837d3f02a3c
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-08 17:45:10 +0200

    Solved the `Circle area` problem

---
+++ Programming fundamentals/Data types/CircleArea/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CircleArea
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            double radius = double.Parse(Console.ReadLine());
+            double area = radius * radius * Math.PI;
+            Console.WriteLine($"{area:f12}");
+        }
+    }
+}
