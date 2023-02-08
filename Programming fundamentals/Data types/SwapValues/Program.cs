cd07e597813905e78ef5035ab4e14a8089073814
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-08 17:38:52 +0200

    Solved the `Swap values` problem

---
+++ Programming fundamentals/Data types/SwapValues/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SwapValues
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Before:");
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine("After:");
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+        }
+    }
+}
