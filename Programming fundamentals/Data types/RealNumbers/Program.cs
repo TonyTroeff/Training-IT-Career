02fe6d9b6e8f5a0fb179f1500dd8fe04e46ce064
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-08 17:43:13 +0200

    Solved the `Real numbers` problem

---
+++ Programming fundamentals/Data types/RealNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RealNumbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            double num1 = 3.141592653589793238;
+            float num2 = 1.60217657f;
+            decimal num3 = 7.8184261974584555216535342341m;
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+        }
+    }
+}
