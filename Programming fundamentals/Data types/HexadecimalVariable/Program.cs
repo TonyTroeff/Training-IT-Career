0cd848eff70c252bde03e87dae38d5f22416be24
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-08 17:36:33 +0200

    Solved the `Hexadecimal variable` problem

---
+++ Programming fundamentals/Data types/HexadecimalVariable/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HexadecimalVariable
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input, 16);
+            Console.WriteLine(number);
+        }
+    }
+}
