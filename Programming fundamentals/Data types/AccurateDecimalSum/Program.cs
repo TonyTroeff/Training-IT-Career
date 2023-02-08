8f38742223e6664c1fdbde003cc7bc7e5e1d1dff
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-08 17:47:16 +0200

    Solved the `Accurate decimal sum` problem

---
+++ Programming fundamentals/Data types/AccurateDecimalSum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AccurateDecimalSum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            decimal sum = 0m;
+            for (int i = 0; i < n; i++)
+            {
+                decimal currentNumber = decimal.Parse(Console.ReadLine());
+                sum += currentNumber;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
