b56c11af1a6c9532fb505919d60357e8905584eb
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-10 22:32:26 +0200

    Solved the `String concatenation` problem

---
+++ Programming fundamentals/Data types/StringConcatenation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace StringConcatenation
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            char separator = char.Parse(Console.ReadLine());
+            string evenOrOdd = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            bool concatenateOnEvenSteps = evenOrOdd == "even";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= n; i++)
+            {
+                string input = Console.ReadLine();
+
+                bool isEvenStep = (i % 2 == 0);
+                if (concatenateOnEvenSteps == isEvenStep)
+                {
+                    sb.Append(input);
+                    sb.Append(separator);
+                }
+            }
+
+            if (sb.Length > 0) sb.Length--;
+            Console.WriteLine(sb);
+        }
+    }
+}
