6f2ecf53a8a76920860d9d5f04005ddbe35a5e84
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-10 22:06:53 +0200

    Solved the `Type ranges` problem

---
+++ Programming fundamentals/Data types/TypeRanges/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TypeRanges
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string type = Console.ReadLine();
+
+            if (type == "int")
+            {
+                Console.WriteLine(int.MaxValue);
+                Console.WriteLine(int.MinValue);
+            }
+            else if (type == "uint")
+            {
+                Console.WriteLine(uint.MaxValue);
+                Console.WriteLine(uint.MinValue);
+            }
+            else if (type == "long")
+            {
+                Console.WriteLine(long.MaxValue);
+                Console.WriteLine(long.MinValue);
+            }
+            else if (type == "byte")
+            {
+                Console.WriteLine(byte.MaxValue);
+                Console.WriteLine(byte.MinValue);
+            }
+            else if (type == "sbyte")
+            {
+                Console.WriteLine(sbyte.MaxValue);
+                Console.WriteLine(sbyte.MinValue);
+            }
+        }
+    }
+}
