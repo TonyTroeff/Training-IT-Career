1197b0d69b48e63bc4f54daaa4a42d1b4baeb1c4
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-10 22:09:16 +0200

    Solved the `Number check` problem

---
+++ Programming fundamentals/Data types/NumberCheck/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumberCheck
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            if (long.TryParse(input, out _)) Console.WriteLine("integer");
+            else Console.WriteLine("floating-point");
+        }
+    }
+}
