1e137d2bc709dbb7d4d5318fbd1374e370afd848
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-10 22:23:09 +0200

    Solved the `Builder` problem

---
+++ Programming fundamentals/Data types/Builder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Builder
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+
+            sbyte sbytePrice;
+            int intPrice;
+            if (sbyte.TryParse(input1, out sbytePrice))
+            {
+                intPrice = int.Parse(input2);
+            }
+            else
+            {
+                sbytePrice = sbyte.Parse(input2);
+                intPrice = int.Parse(input1);
+            }
+
+            long total = 10L * intPrice + 4 * sbytePrice;
+            Console.WriteLine(total);
+        }
+    }
+}
