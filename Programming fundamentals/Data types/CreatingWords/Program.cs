baa44d3adf04d7bc9401dbf3a4372db0634aefb3
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-10 22:25:50 +0200

    Solved the `CreatingWords` problem

---
+++ Programming fundamentals/Data types/CreatingWords/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace CreatingWords
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder(capacity: n);
+            for (int i = 0; i < n; i++)
+            {
+                char currentSymbol = char.Parse(Console.ReadLine());
+                sb.Append(currentSymbol);
+            }
+
+            Console.WriteLine($"The word is: {sb}");
+        }
+    }
+}
