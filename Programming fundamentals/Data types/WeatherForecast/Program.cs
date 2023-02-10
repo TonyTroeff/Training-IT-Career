a36d2c96713b93805164a009e0c1f390c353bb0a
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-10 22:12:01 +0200

    Solved the `Weather forecast` problem

---
+++ Programming fundamentals/Data types/WeatherForecast/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WeatherForecast
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            if (sbyte.TryParse(input, out _)) Console.WriteLine("Sunny");
+            else if (int.TryParse(input, out _)) Console.WriteLine("Cloudy");
+            else if (long.TryParse(input, out _)) Console.WriteLine("Windy");
+            else Console.WriteLine("Rainy");
+        }
+    }
+}
