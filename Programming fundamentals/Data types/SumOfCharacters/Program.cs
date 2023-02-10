876c42ded026717e62a139132bc8eab798a66515
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-10 22:27:53 +0200

    Solved the `Sum of characters` problem

---
+++ Programming fundamentals/Data types/SumOfCharacters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumOfCharacters
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                char currentSymbol = char.Parse(Console.ReadLine());
+                sum += currentSymbol;
+            }
+
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
