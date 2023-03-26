3509e335cf02059eaecdccda08b33452ea58992c
Tony Troeff <tonyslav_troeff@outlook.com>
2023-03-27 00:44:16 +0300

    Solved the `Population counting` problem

---
+++ Programming fundamentals/Dictionaries/PopulationCounting/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PopulationCounting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> dataByCountry = new Dictionary<string, Dictionary<string, int>>();
+            Dictionary<string, int> totalPopulationByCountry = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+            while (input != "report")
+            {
+                string[] data = input.Split('|');
+                string town = data[0];
+                string country = data[1];
+                int population = int.Parse(data[2]);
+
+                if (!dataByCountry.ContainsKey(country))
+                {
+                    dataByCountry[country] = new Dictionary<string, int>();
+                    totalPopulationByCountry[country] = 0;
+                }
+
+                dataByCountry[country][town] = population;
+                totalPopulationByCountry[country] += population;
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var (country, totalPopulation) in totalPopulationByCountry.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"{country} (total population: {totalPopulation})");
+
+                foreach (var (town, townPopulation) in dataByCountry[country].OrderByDescending(x => x.Value))
+                    Console.WriteLine($"=>{town}: {townPopulation}");
+            }
+        }
+    }
+}
